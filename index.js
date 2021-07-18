8b9ee0fcfd6fdba63c31114f3ff174d298db1e84
unknown <intwarisymplice@gmail.com>
2021-07-18 19:51:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import usersRoutes from './routes/albums.js';
+
+const app = express();
+const PORT = 1010;
+
+app.use(bodyParser.json());
+app.use('/albums', usersRoutes);
+app.get('/', (req, res) => {
+res.send('hello from api homepage');
+});
+app.listen(PORT, () => console.log(`Server running on port: http://localhost: ${PORT}`));
